fc84c8b9f12733b5fb0c31f5d8a8b6e2e6c6a72d
ljq <ljq@ljqdeiMac-Pro.local>
2019-07-24 21:12:43 +0800

    0.0.1
    创建swift版本工具类工程

---
+++ DevSwiftToolsProject/DevSwiftToolsProject/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  DevSwiftToolsProject
+//
+//  Created by ljq on 2019/7/24.
+//  Copyright © 2019 ljq. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
